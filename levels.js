8498c1d3695db89884bfc6d24d2fedc7ac129d20
will.demarest@gmail.com <will@Williams-MacBook-Air.local>
2018-10-15 16:04:10 -0500

    new game working

---
+++ levels.js
@@ -0,0 +1,15 @@
+function MakeLevelList(){
+	let levelList = [
+		[
+		{type: "blower", x: 0, y: 0},
+		{type: "payload", x: 0, y: 200},
+		{type: "destination", x: 0, y: 800},
+		],
+		[
+		{type: "blower", x: 200, y: 200},
+		{type: "payload", x: 0, y: 100},
+		{type: "destination", x: 0, y: 300},
+		],
+	]
+	return levelList;
+}
