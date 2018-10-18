f949e2580a4a1d5f8a23cd917452fb4b3bac7407
wdemarest <will.demarest@gmail.com>
2018-10-17 20:24:04 -0500

    alpha 1

--- levels.js
+++ levels.js
@@ -1,16 +1,145 @@
+function EditLevel(sim, levelPlan, type, x, y){
+	if(type == "deletePiece"){
+		levelPlan.pop()
+	}else{
+		levelPlan.push({type: type, x: x, y: y})
+		sim.addPiece(x, y, type)
+	}
+}
 function MakeLevelList(){
-	let levelList = [
-		[
+	let LevelList = [
+		[//Level Builder
 			{type: "blower", x: 0, y: 0},
-			{type: "payload", x: 0, y: 200},
-			{type: "destination", x: 0, y: 800},
-			{type: "block", x: 200, y: 800},
+			{type: "destination", x: -200, y: 0},
+			{type: "payload", x: 200, y: 0},
+
+			{type: "bomb", x: 0, y: 200},
+
+			{type: "block", x: 50, y: -150},
+			{type: "block", x: 50, y: -250},
+			{type: "block", x: -50, y: -150},
+			{type: "block", x: -50, y: -250},
 		],
-		[
-			{type: "blower", x: 200, y: 200},
-			{type: "payload", x: 0, y: 100},
+		[//Minefield
+			{type: "blower", x: 0, y: -100},
 			{type: "destination", x: 0, y: 300},
+			{type: "payload", x: 0, y: -50},
+
+			{type: "bomb", x: 0, y: 0},
+			{type: "bomb", x: 0, y: 50},
+			{type: "bomb", x: 0, y: 100},
+			{type: "bomb", x: 0, y: 150},
+			{type: "bomb", x: 0, y: 200},
+
+			{type: "bomb", x: 50, y: 0},
+			{type: "bomb", x: 50, y: 50},
+			{type: "bomb", x: 50, y: 100},
+			{type: "bomb", x: 50, y: 150},
+			{type: "bomb", x: 50, y: 200},
+
+			{type: "bomb", x: 100, y: 0},
+			{type: "bomb", x: 100, y: 50},
+			{type: "bomb", x: 100, y: 100},
+			{type: "bomb", x: 100, y: 150},
+			{type: "bomb", x: 100, y: 200},
+
+			{type: "bomb", x: -50, y: 0},
+			{type: "bomb", x: -50, y: 50},
+			{type: "bomb", x: -50, y: 100},
+			{type: "bomb", x: -50, y: 150},
+			{type: "bomb", x: -50, y: 200},
+
+			{type: "bomb", x: -100, y: 0},
+			{type: "bomb", x: -100, y: 50},
+			{type: "bomb", x: -100, y: 100},
+			{type: "bomb", x: -100, y: 150},
+			{type: "bomb", x: -100, y: 200},
+
+			{type: "block", x: -100, y: -200},
+			{type: "block", x: 0, y: -200},
+			{type: "block", x: 100, y: -200},
+
+			{type: "block", x: -100, y: 400},
+			{type: "block", x: 0, y: 400},
+			{type: "block", x: 100, y: 400},
+
+			{type: "block", x: -200, y: -200},
+			{type: "block", x: -200, y: -100},
+			{type: "block", x: -200, y: 0},
+			{type: "block", x: -200, y: 100},
+			{type: "block", x: -200, y: 200},
+			{type: "block", x: -200, y: 300},
+			{type: "block", x: -200, y: 400},
+
+			{type: "block", x: 200, y: -200},
+			{type: "block", x: 200, y: -100},
+			{type: "block", x: 200, y: 0},
+			{type: "block", x: 200, y: 100},
+			{type: "block", x: 200, y: 200},
+			{type: "block", x: 200, y: 300},
+			{type: "block", x: 200, y: 400},
+		],
+		[//Trap
+			{type: "blower", x: 0, y: -100},
+			{type: "destination", x: 0, y: 200},
+			{type: "payload", x: 0, y: 0},
+
+			{type: "bomb", x: 40, y: 0},
+			{type: "bomb", x: 0, y: 40},
+			{type: "bomb", x: -40, y: 0},
+			{type: "bomb", x: 0, y: -40},
+
+			{type: "bomb", x: 35, y: 35},
+			{type: "bomb", x: -35, y: -35},
+			{type: "bomb", x: -35, y: 35},
+			{type: "bomb", x: 35, y: -35},
+		],
+		[//Tunnel
+			{type: "blower", x: 0, y: 0},
+			{type: "destination", x: 200, y: 100},
+			{type: "payload", x: 0, y: 50},
+			{type: "block", x: -50, y: -75},
+			{type: "block", x: 50, y: -75},
+
+			{type: "block", x: -100, y: 0},
+			{type: "block", x: -100, y: 100},
+			{type: "block", x: -100, y: 200},
+			{type: "block", x: -100, y: 300},
+			{type: "block", x: -100, y: 400},
+			{type: "block", x: 0, y: 400},
+			{type: "block", x: 100, y: 400},
+			{type: "block", x: 200, y: 400},
+
+			{type: "block", x: 100, y: 0},
+			{type: "block", x: 100, y: 100},
+			{type: "block", x: 100, y: 200},
+			{type: "block", x: 200, y: 200},
+
+			{type: "block", x: 200, y: 0},
+			{type: "block", x: 300, y: 25},
+			{type: "block", x: 300, y: 175},
+		],
+		[//Fun
+			{type: "blower", x: 0, y: 0},
+			{type: "destination", x: 0, y: 200},
+
+			{type: "bomb", x: 100, y: 0},
+			{type: "bomb", x: 0, y: 100},
+			{type: "bomb", x: -100, y: 0},
+			{type: "bomb", x: 0, y: -100},
+
+			{type: "bomb", x: 75, y: 75},
+			{type: "bomb", x: -75, y: -75},
+			{type: "bomb", x: -75, y: 75},
+			{type: "bomb", x: 75, y: -75},
+
+			{type: "block", x: 75, y: 0},
+			{type: "block", x: -75, y: 0},
+			{type: "block", x: 0, y: 75},
+			{type: "block", x: 0, y: -75},
 		],
 	]
-	return levelList;
+	return LevelList;
 }
+
+//Blower 50, Payload 40, Destination 75, Bomb 30, Block 100
